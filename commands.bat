8b97e601222ce7104a50f9a74406eac10bb7bf25
austin <austinlee199723@yahoo.com>
2021-07-16 14:49:42 -0700

    detach from channel

--- commands.bat
+++ commands.bat
@@ -1,3 +1,2 @@
-git add .
-git commit -m "detach from channel"
-git push origin master
+cd client 
+npm run build && cd .. && git add . && git commit -m "detach from channel" && git push origin master
